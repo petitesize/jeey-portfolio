9e431759f6b914fe61df27cb7f659db983e66ab3
petitesize <petitesize22@gmail.com>
2024-12-27 18:13:48 +0900

    jwiiter 사이트 방문 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -109,11 +109,13 @@ export const projectDetailData = [
     img: Jwitter,
     title: "X(Twitter) 클론 코딩",
     buttons: [
+      { text: "사이트 방문", link: "https://jwitter-bf762.web.app/" },
       { text: "GitHub", link: "https://github.com/petitesize/Jwitter" },
     ],
     desc: [
       "Firebase를 활용법을 익히기 위한 X(Twitter) 클론 코딩",
       "로그인, 회원가입 및 게시물 CRUD 등의 SNS 기본 기능을 X(Twitter)를 클론 코딩하여 구현한 웹사이트",
+      "Firebase Storage를 통해 게시물에 이미지 업로드가 가능합니다.",
     ],
 
     detailsDesc: [
